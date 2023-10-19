4330f650a320276d87697cbf8de37aeb84599c10
yarosshest <yarosshest@gmail.com>
2023-10-19 03:44:08 +0300

    authorization debug

--- app/templates/login.js
+++ app/templates/login.js
@@ -1,49 +1,24 @@
+const handleSubmit = (event) => {
+  event.preventDefault();
 
-document.getElementById('form').addEventListener("submit", (e) => {
-  let form = document.getElementById('form');
-  form.action = '/token/';
-  form.method = 'POST';
-});
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/token/", {
+    credentials: 'include',
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+  .then((response) => response.json())
+  .then((json) => {
+    document.cookie = 'access_token=' + json['access_token'];
+    document.cookie = 'token_type=' + json['token_type'];
+  }).catch((err) => {
+    console.log(err);
+  });
+};
 
-const postData = async (url = '') => {
-  let form = document.getElementById('form');
-  form.action = '/token/';
-  form.method = 'POST';
-  // form.innerHTML += '<input name="grant_type" value="password">';
-  form.submit();
-
-  // let formData = new FormData();
-  // formData.append('grant_type', 'password');
-  // formData.append('username', document.getElementById('username').value);
-  // formData.append('password', document.getElementById('password').value);
-
-  // form.formData
-  // // Формируем запрос
-  // const response = await fetch(url, {
-  //   // Метод, если не указывать, будет использоваться GET
-  //   method: 'POST',
-  //   // redirect: 'follow',
-  //  // Заголовок запроса
-  //   // body: new URLSearchParams(formData),
-  //   body: new URLSearchParams (formData),
-  //   headers: {
-  //     'Accept': 'application/json, text/plain, */*',
-  //     'Content-Type': 'application/json'
-  //   },
-  // }).then((responseJson) => {
-  //   if(responseJson.status == 401){
-  //     document.getElementById("err").textContent="Incorrect username or password";
-  //   }
-  //   if(responseJson.ok){
-  //     window.location.href = responseJson.url;
-  //   }
-  // }).catch((error) => {
-  //   document.getElementById("err").textContent=res.json()["message"];
-  // });
-  // return response;
-}
-
-document.querySelector(".login-btn").addEventListener("click", (e)=>{
-    postData('/token/')
-      }
-    )
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
